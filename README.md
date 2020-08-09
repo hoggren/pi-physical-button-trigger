c96e1fc0b897460113726a3865118803a71d51c4
Patrik Hoggren <p@hoggren.nu>
2020-08-09 08:43:46 +0200

    :rocket: Initial commmit

---
+++ README.md
@@ -0,0 +1,28 @@
+# pi-physical-button-trigger
+
+Developed on a Raspberry Pi 4 board using library **gpiozero**.
+
+## Run command on Raspberry Pi from a physical button
+
+See [`gpiozero documentation`](https://gpiozero.readthedocs.io/en/stable/index.html) :link:  for the required GPIO input and components.
+
+### Get started
+
+All required packages should be installed on a default Raspian installation, so just run it:
+
+```shell
+# Might need to make the file executable by running:
+# chmod u+x main
+
+./main ls
+```
+
+### License
+
+MIT
+
+Do what you want - don't blame me.
+
+### Author
+
+info@hoggren.nu
---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from subprocess import PIPE, run
+from argparse import ArgumentParser
+from gpiozero import Button
+from signal import pause
+
+
+parser = ArgumentParser('physical_button')
+parser.add_argument('cmd', help="Command to run as argument")
+args = parser.parse_args()
+
+print('Running PWD led test script...\n')
+
+
+def button_pressed():
+    print(f'Running command: \'{args}\'')
+
+    if len(args.cmd):
+        proc = run(args.cmd, stdout=PIPE, stderr=PIPE)
+
+    if len(proc.stdout) > 0:
+        print(proc.stdout)
+
+
+button = Button(2)
+
+button.when_pressed = button_pressed
+pause()
